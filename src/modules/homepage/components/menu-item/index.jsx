4a7647193c570dda689923827de67f78d9064fac
cody0203 <congdinh2395@gmail.com>
2019-11-29 21:41:06 +0700

    add navigate to menu-item

--- src/modules/homepage/components/menu-item/index.jsx
+++ src/modules/homepage/components/menu-item/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import classes from './index.module.scss';
+import {withRouter} from 'react-router-dom';
 
 const MenuItem = props => {
-  const {title, imageUrl, size} = props;
+  const {title, imageUrl, size, history, linkUrl, match} = props;
+  const directUrl = () => {
+    history.push(`${match.url}${linkUrl}`);
+  };
   return (
-    <div className={[classes['menu-item'], classes[size]].join(' ')}>
+    <div
+      className={[classes['menu-item'], classes[size]].join(' ')}
+      onClick={directUrl}
+    >
       <div
         className={classes['background-image']}
         style={{
@@ -19,4 +26,4 @@ const MenuItem = props => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
